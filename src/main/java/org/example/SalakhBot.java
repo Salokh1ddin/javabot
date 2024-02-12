212847563a1ddc56aa83db620a7b0a054cd85d2c
Salakh1ddin <usad6409@gmail.com>
2024-02-12 17:42:40 +0500

    second commit

--- src/main/java/org/example/SalakhBot.java
+++ src/main/java/org/example/SalakhBot.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -15,28 +16,46 @@ public class SalakhBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
-        Message message = update.getMessage();
-        List<PhotoSize> photoList = message.getPhoto();
-        System.out.println(photoList);
-        for (PhotoSize photoSize : photoList) {
-            System.out.println(photoSize);
-        }
-        String caption = message.getCaption();
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
 
-        PhotoSize photoSize = photoList.get(photoList.size() - 1);
-        photoSize.getFileId();
-        String fileId = photoSize.getFileId();
+            if (message.hasText()) {
+                if (message.getText().equalsIgnoreCase("/start")) {
+                    SendMessage sendMessage = new SendMessage(
+                            message.getChatId().toString(),
+                            "Assalomu alaykum"
+                    );
 
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(message.getChatId());
-        sendPhoto.setPhoto(new InputFile(fileId));
-        sendPhoto.setCaption(caption + " nma qlay o'zing pishirgan osh ozin yegin");
+                    try {
+                        execute(sendMessage);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            } else if (message.hasPhoto()) {
+                System.out.println(update);
+                List<PhotoSize> photoList = message.getPhoto();
+                System.out.println(photoList);
+                for (PhotoSize photoSize : photoList) {
+                    System.out.println(photoSize);
+                }
+                String caption = message.getCaption();
 
-        try {
-            execute(sendPhoto);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+                PhotoSize photoSize = photoList.get(photoList.size() - 1);
+                photoSize.getFileId();
+                String fileId = photoSize.getFileId();
+
+                SendPhoto sendPhoto = new SendPhoto();
+                sendPhoto.setChatId(message.getChatId());
+                sendPhoto.setPhoto(new InputFile(fileId));
+                sendPhoto.setCaption(caption + " nma qlay o'zing pishirgan osh ozin yegin");
+
+                try {
+                    execute(sendPhoto);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
     }
